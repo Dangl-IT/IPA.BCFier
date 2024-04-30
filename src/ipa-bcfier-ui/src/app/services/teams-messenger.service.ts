ec0f4a3b1484de822e4542bf14965e7993b763ca
Borys91 <borisilchenko1991@gmail.com>
2024-04-30 15:39:11 +0300

    connected sending messsage to Teams with backend

--- src/ipa-bcfier-ui/src/app/services/teams-messenger.service.ts
+++ src/ipa-bcfier-ui/src/app/services/teams-messenger.service.ts
@@ -1,7 +1,19 @@
-import { Injectable, inject } from '@angular/core';
-import { combineLatestWith, take } from 'rxjs';
+import { Injectable, OnDestroy, inject } from '@angular/core';
+import {
+  EMPTY,
+  Subject,
+  combineLatestWith,
+  switchMap,
+  take,
+  takeUntil,
+} from 'rxjs';
 import { TopicMessengerService } from './topic-messenger.service';
 import { SettingsMessengerService } from './settings-messenger.service';
+import {
+  TeamsMessagePost,
+  TeamsMessagesClient,
+} from '../generated-client/generated-client';
+import { SelectedProjectMessengerService } from './selected-project-messenger.service';
 
 export enum MessageType {
   AddComment = 'AddNomment',
@@ -11,10 +23,27 @@ export enum MessageType {
 @Injectable({
   providedIn: 'root',
 })
-export class TeamsMessengerService {
+export class TeamsMessengerService implements OnDestroy {
   private topicMessengerService = inject(TopicMessengerService);
   private settingsMessengerService = inject(SettingsMessengerService);
-  constructor() {}
+  private selectedProjectMessengerService = inject(
+    SelectedProjectMessengerService
+  );
+  private teamsMessagesClient = inject(TeamsMessagesClient);
+
+  private destroyed$ = new Subject<void>();
+  private projectId = '';
+  constructor() {
+    this.selectedProjectMessengerService.selectedProject
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe((p) => {
+        this.projectId = p?.id || '';
+      });
+  }
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 
   sendMessageToTeams(messageType: MessageType): void {
     switch (messageType) {
@@ -33,30 +62,73 @@ export class TeamsMessengerService {
   }
 
   private sendInfoAboutAddComment(): void {
-    this.topicMessengerService.selectedTopic.pipe(take(1)).subscribe((t) => {
-      const lastAddedComment = t?.comments[t?.comments.length - 1];
-      console.log(lastAddedComment?.author);
-      console.log(lastAddedComment?.text);
-      //TODO Send data to new backend method
-    });
+    this.topicMessengerService.selectedTopic
+      .pipe(
+        take(1),
+        switchMap((t) => {
+          const lastAddedComment = t?.comments[t?.comments.length - 1];
+          if (this.projectId && lastAddedComment?.author && t?.id) {
+            const model: TeamsMessagePost = {
+              comment: lastAddedComment?.text,
+              username: lastAddedComment?.author,
+            };
+            return this.teamsMessagesClient.announceNewCommentInProjectTopic(
+              this.projectId,
+              t?.id,
+              model
+            );
+          } else {
+            return EMPTY;
+          }
+        })
+      )
+      .subscribe(() => {});
   }
 
   private sendInfoAboutAddViewpoint(): void {
     this.topicMessengerService.selectedTopic
-      .pipe(take(1), combineLatestWith(this.settingsMessengerService.settings))
-      .subscribe(([t, s]) => {
-        const lastAddedViewpoint = t?.viewpoints[t?.viewpoints.length - 1];
-        console.log(lastAddedViewpoint?.snapshotBase64);
-        console.log(s.username);
-        //TODO Send data to new backend method
-      });
+      .pipe(
+        take(1),
+        combineLatestWith(this.settingsMessengerService.settings),
+        switchMap(([t, s]) => {
+          if (this.projectId && t?.id) {
+            const lastAddedViewpoint = t?.viewpoints[t?.viewpoints.length - 1];
+            const model: TeamsMessagePost = {
+              viewpointBase64: lastAddedViewpoint.snapshotBase64,
+              username: s.username,
+            };
+            return this.teamsMessagesClient.announceNewCommentInProjectTopic(
+              this.projectId,
+              t?.id,
+              model
+            );
+          }
+          return EMPTY;
+        })
+      )
+      .subscribe(() => {});
   }
 
   private sendInfoAboutChangeTitle(): void {
-    this.topicMessengerService.selectedTopic.pipe(take(1)).subscribe((t) => {
-      console.log(t?.title);
-      console.log(t?.creationAuthor);
-      //TODO Send data to new backend method
-    });
+    this.topicMessengerService.selectedTopic
+      .pipe(
+        take(1),
+        switchMap((t) => {
+          if (this.projectId && t?.id && t.creationAuthor) {
+            const model: TeamsMessagePost = {
+              topicTitle: t.title,
+              username: t.creationAuthor,
+            };
+            return this.teamsMessagesClient.announceNewCommentInProjectTopic(
+              this.projectId,
+              t?.id,
+              model
+            );
+          }
+          return EMPTY;
+        })
+      )
+
+      .subscribe(() => {});
   }
 }
