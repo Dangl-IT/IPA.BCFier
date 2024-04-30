72f6f689bfe7c74ad0f3a870a93a395c25934602
Georg Dangl <georg@dangl.me>
2024-04-30 12:16:51 +0200

    Add backend to send teams messages via webhook

---
+++ src/IPA.Bcfier.App/Models/Services/TeamsMessages/TeamsMessage.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace IPA.Bcfier.App.Models.Services.TeamsMessages
+{
+    public class TeamsMessage
+    {
+        [JsonProperty("@type")]
+        public string Type { get; } = "MessageCard";
+
+        [JsonProperty("@context")]
+        public string Context { get; } = "http://schema.org/extensions";
+
+        [JsonProperty("themeColor")]
+        public string ThemeColor { get; } = "0076D7";
+        //public string ThemeColor { get; } = "eed300";
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; } = string.Empty;
+
+        [JsonProperty("sections")]
+        public List<TeamsSection> Sections { get; set; } = new List<TeamsSection>();
+    }
+}
---
+++ src/IPA.Bcfier.App/Models/Services/TeamsMessages/TeamsSection.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace IPA.Bcfier.App.Models.Services.TeamsMessages
+{
+    public class TeamsSection
+    {
+        [JsonProperty("activityTitle")]
+        public string? ActivityTitle { get; set; }
+
+        [JsonProperty("activitySubtitle")]
+        public string? ActivitySubtitle { get; set; }
+
+        [JsonProperty("facts")]
+        public List<TeamsFact>? Facts { get; set; }
+
+        [JsonProperty("images")]
+        public List<TeamsImage>? Images { get; set; }
+    }
+}
