3cc202bdfd9332f7342c7e3e6e5773bc98334d8d
Borys91 <borisilchenko1991@gmail.com>
2025-07-16 17:07:19 +0300

    fixed tests

--- src/ipa-bcfier-ui/src/app/components/clash-grouping-options/clash-grouping-options.component.spec.ts
+++ src/ipa-bcfier-ui/src/app/components/clash-grouping-options/clash-grouping-options.component.spec.ts
@@ -2,15 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ClashGroupingOptionsComponent } from './clash-grouping-options.component';
 
+import { AppTestingModule } from '../../app.testing.module';
+
 describe('ClashGroupingOptionsComponent', () => {
   let component: ClashGroupingOptionsComponent;
   let fixture: ComponentFixture<ClashGroupingOptionsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ClashGroupingOptionsComponent]
-    })
-    .compileComponents();
+      imports: [ClashGroupingOptionsComponent, AppTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ClashGroupingOptionsComponent);
     component = fixture.componentInstance;
--- src/ipa-bcfier-ui/src/app/components/selected-edit-topic/selected-edit-topic.component.spec.ts
+++ src/ipa-bcfier-ui/src/app/components/selected-edit-topic/selected-edit-topic.component.spec.ts
@@ -2,15 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SelectedEditTopicComponent } from './selected-edit-topic.component';
 
+import { AppTestingModule } from '../../app.testing.module';
+
 describe('SelectedEditTopicComponent', () => {
   let component: SelectedEditTopicComponent;
   let fixture: ComponentFixture<SelectedEditTopicComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SelectedEditTopicComponent]
-    })
-    .compileComponents();
+      imports: [SelectedEditTopicComponent, AppTestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SelectedEditTopicComponent);
     component = fixture.componentInstance;
