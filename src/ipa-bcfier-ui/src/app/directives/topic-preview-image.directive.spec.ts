0abf3e9886bac374e7c6260ab66188600ca25907
ruslan-minukov <ruslan.minukov@qit.software>
2025-09-16 14:48:54 +0300

    fix: fixed tests

--- src/ipa-bcfier-ui/src/app/directives/topic-preview-image.directive.spec.ts
+++ src/ipa-bcfier-ui/src/app/directives/topic-preview-image.directive.spec.ts
@@ -1,10 +1,23 @@
 import { ElementRef } from '@angular/core';
 import { TopicPreviewImageDirective } from './topic-preview-image.directive';
+import { TestBed } from '@angular/core/testing';
 
 describe('TopicPreviewImageDirective', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ElementRef,
+          useValue: {}
+        },
+      ],
+    }).compileComponents();
+  });
+  
   it('should create an instance', () => {
-    const el = new ElementRef<HTMLDivElement>(document.createElement('div'));
-    const directive = new TopicPreviewImageDirective(el);
-    expect(directive).toBeTruthy();
+    TestBed.runInInjectionContext(() => {
+      const directive = new TopicPreviewImageDirective();
+      expect(directive).toBeTruthy();
+    });
   });
 });
