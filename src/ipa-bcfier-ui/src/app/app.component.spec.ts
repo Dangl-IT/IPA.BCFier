30436486eb25f63d22cc875adc59ddf421881939
ruslan-minukov <ruslan.minukov@qit.software>
2025-12-15 15:28:15 +0200

    fix: avoid connecting SignalR for test

--- src/ipa-bcfier-ui/src/app/app.component.spec.ts
+++ src/ipa-bcfier-ui/src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { AppTestingModule } from './app.testing.module';
+import { BcfierHubConnectorService } from './services/connectors/bcfier-hub-connector.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent, AppTestingModule],
+      providers: [{ provide: BcfierHubConnectorService, useValue: {} }],
     }).compileComponents();
   });
 
